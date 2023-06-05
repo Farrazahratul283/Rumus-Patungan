4750a2cb10f9489d6c67fd5dbe4b0e2b6859dc6f
Farrazahratul283 <farrazahratul66@gmail.com>
2023-06-05 12:47:12 +0700

    FarraUTS

---
+++ UTS.java
@@ -0,0 +1,13 @@
+import java.text.Format;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String nama ="Farra Zahratul Milla";
+        int nim =2113010283;
+
+        String pesan = String.format("Hallo teman-teman nama saya ",nama,nim);
+
+        System.out.println(pesan);
+    }
+}
